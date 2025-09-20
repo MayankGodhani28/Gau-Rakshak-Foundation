274d3e54a756a12b2b6dc64744900a11a173f684
Mayank Godhani <mayankgodhani222@gmail.com>
2025-09-20 17:04:17 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -14,11 +14,14 @@ const path = require("path");
 const authRoutes=require("./routes/auth.js");
 
 
+
 // Initialize express BEFORE using app
 const app = express();
 
 // ✅ Serve "images" folder as static
 app.use("/images", express.static(path.join(__dirname, "datax/images")));
+app.use(express.static(path.join(__dirname, "../client/build")));
+
 
 // Middleware
 app.use(cors());
@@ -55,3 +58,9 @@ mongoose
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
+// React frontend fallback (must be after API routes)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+});
+
